d8a814e5536509e7e13a25fb9587f64d7d344e19
Harsh <hjain_be21@thapar.edu>
2023-02-02 03:48:56 +0530

    added collapsible cards

--- analystt.ai/src/components/Card.js
+++ analystt.ai/src/components/Card.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import Collapsible from 'react-collapsible'
 
 const Card = (props) => {
   return (
     <div id="card">
+        <div id="main_card">
         <span id="detail_1">Hello <b>{props.name} !</b></span>
         <span id="detail_2"><b>Username</b> <br/>{props.username}</span>
         <span id="detail_3"><b>Address</b><br/>{props.add}</span>
         <span id="detail_4"><b>Email</b> <br/>{props.email}</span>
+        </div>
         <br />
-        <button>More</button>
+        <div id="more_info">
+        <Collapsible trigger="More">
+        More
+        hello
+        </Collapsible>
+        </div>
     </div>
   )
 }
