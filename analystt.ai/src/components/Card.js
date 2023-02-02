f462dacfa661ea94a3b3ebe081e8ed74fa40b446
Harsh <hjain_be21@thapar.edu>
2023-02-02 22:10:57 +0530

    added pgnos,minor changes

--- analystt.ai/src/components/Card.js
+++ analystt.ai/src/components/Card.js
@@ -3,20 +3,20 @@ import Collapsible from 'react-collapsible';
 
 const Card = (props) => {
 
-  const [value,setvalue]=useState("More info");
-  const [style,setstyle]=useState({
-    textDecoration:'underline',
-    fontStyle:'italic'
+  const [value, setvalue] = useState("More info");
+  const [style, setstyle] = useState({
+    textDecoration: 'underline',
+    fontStyle: 'italic'
   })
-  
-  const toggle_open=()=>{
-    if(value=="More info"){
-    setvalue("Show less");
+
+  const toggle_open = () => {
+    if (value == "More info") {
+      setvalue("Show less");
     }
   }
-  const toggle_close=()=>{
-    if(value=="Show less"){
-    setvalue("More info");
+  const toggle_close = () => {
+    if (value == "Show less") {
+      setvalue("More info");
     }
   }
   return (
@@ -29,31 +29,31 @@ const Card = (props) => {
           <span id="detail_4" className='info'><b>Company</b> <br />{props.company}</span>
         </div>
       </div>
-      
+
       <Collapsible trigger={value} onTriggerOpening={toggle_open} onTriggerClosing={toggle_close} triggerStyle={style}>
-        
+
         <div id="more_info">
           <h3><u>Description</u></h3>
           <span id="desc_heading">{props.company}</span>
           <br />
           <div id="desc_row">
             <div id="desc_col_1">
-            <b><i>Contact person:</i> </b>{props.name}
-            <br /><br/>
-            <b><i>Phone:</i></b> {props.phone}
-            <br/><br/>
-            <b><i>Email :</i></b> {props.email}
+              <b><i>Contact person:</i> </b>{props.name}
+              <br /><br />
+              <b><i>Phone:</i></b> {props.phone}
+              <br /><br />
+              <b><i>Email :</i></b> {props.email}
             </div>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <div id="desc_col_2">
-            <b><i>Company goals:</i></b> {props.desc}
-            <br/><br/>
-            <b><i>Company Website:</i></b> {props.website}
-            <br /><br/>
-            <b><i>Company address:</i> </b>{props.add_street}, {props.add_suite}, {props.add_city}
-            </div>
+              <b><i>Company goals:</i></b> {props.desc}
+              <br /><br />
+              <b><i>Company Website:</i></b> {props.website}
+              <br /><br />
+              <b><i>Company address:</i> </b>{props.add_street}, {props.add_suite}, {props.add_city}
             </div>
           </div>
+        </div>
       </Collapsible>
     </>
   )
--- analystt.ai/src/components/Cardspace.js
+++ analystt.ai/src/components/Cardspace.js
@@ -2,90 +2,91 @@ import React, { useEffect, useState } from 'react'
 import Card from './Card';
 
 const Cardspace = () => {
-    
-    const [details,setdetails]=useState([]);
-    const [currentPage,setcurrentPage]=useState(1);
-    const [postsPerPage,setpostsPerPage]=useState(3);
 
-    const [isLoading,setisLoading]=useState(true);
+    const [details, setdetails] = useState([]);
+    const [currentPage, setcurrentPage] = useState(1);
+    const [postsPerPage, setpostsPerPage] = useState(3);
 
-    const fetch_data=async()=>{
-        try{
-            let url="https://jsonplaceholder.typicode.com/users";
+    const [isLoading, setisLoading] = useState(true);
 
-            const res=await fetch(url);
+    const fetch_data = async () => {
+        try {
+            let url = "https://jsonplaceholder.typicode.com/users";
+
+            const res = await fetch(url);
             const data = await res.json();
             setisLoading(false);
             setdetails(data);
             console.log(data);
-        }catch(error){
+        } catch (error) {
             console.log("error");
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch_data();
-    },[]);
+    }, []);
 
-    const lastPostIndex=currentPage*postsPerPage;
-    const firstPostIndex=lastPostIndex-postsPerPage;
-    const currentPosts=details.slice(firstPostIndex,lastPostIndex)
+    const lastPostIndex = currentPage * postsPerPage;
+    const firstPostIndex = lastPostIndex - postsPerPage;
+    const currentPosts = details.slice(firstPostIndex, lastPostIndex)
 
-    const prevPage=()=>{
-        if(currentPage==1){
+    const prevPage = () => {
+        if (currentPage == 1) {
             alert("No pages before this")
         }
-        else{
-            setcurrentPage(currentPage-1);
+        else {
+            setcurrentPage(currentPage - 1);
         }
     }
-    const nextPage=()=>{
-        if(currentPage==4){
+    const nextPage = () => {
+        if (currentPage == 4) {
             alert("No pages after this")
         }
-        else{
-            setcurrentPage(currentPage+1);
+        else {
+            setcurrentPage(currentPage + 1);
         }
     }
 
 
- if (isLoading){
+    if (isLoading) {
+        return (
+            <>
+                <h1 align="center">Loading...</h1>
+            </>
+        )
+    }
     return (
         <>
-        <h1 align="center">Loading...</h1>
+            <div id="prev_next_btn">
+                <button id="prev" onClick={prevPage}>Prev</button>
+                <span id="pageno">{currentPage}/4</span>
+                <button id="next" onClick={nextPage}>Next</button>
+            </div>
+            <div id="container">
+                <div className="card_details">
+                    {
+                        currentPosts.map((element) => {
+                            return (
+                                <Card
+                                    name={element.name}
+                                    username={element.username}
+                                    add_street={element.address.street}
+                                    add_suite={element.address.suite}
+                                    add_city={element.address.city}
+                                    email={element.email}
+                                    phone={element.phone}
+                                    company={element.company.name}
+                                    desc={element.company.bs}
+                                    website={element.website}
+                                />
+                            )
+                        })
+                    }
+                </div>
+            </div>
         </>
     )
- }
-  return (
-    <>
-        <div id="prev_next_btn">
-            <button id="prev" onClick={prevPage}>Prev</button>
-            <button id="next" onClick={nextPage}>Next</button>
-        </div>
-        <div id="container">
-            <div className="card_details">
-                {
-                    currentPosts.map((element)=>{
-                        return(
-                            <Card 
-                            name={element.name}
-                            username={element.username} 
-                            add_street={element.address.street} 
-                            add_suite={element.address.suite}
-                            add_city={element.address.city}
-                            email={element.email} 
-                            phone={element.phone}
-                            company={element.company.name}
-                            desc={element.company.bs}
-                            website={element.website}
-                            />
-                        )
-                    })
-                }
-            </div>
-        </div>
-    </>
-  )
 }
 
 export default Cardspace
